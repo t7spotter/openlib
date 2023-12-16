95f89368c771c4fe198f554d0f829244e5cb9155
Mohamadali Firoozi <alifiroozi@icloud.com>
2023-12-16 21:25:06 +0330

    Added title route

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
+import requests
 from flask import Flask
+from icecream import ic
+import http
 
 
 app = Flask(__name__)
 
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+@app.route("/title/<title>")
+def title(title):
+    parsed = title.replace("%20", "+")
+    response = requests.get(
+        f"https://openlibrary.org/search.json?title={parsed}"
+    ).json()
+    b = []
+    for i in response["docs"]:
+        b.append(i)
+
+    result = []
+    for ii in b:
+        info = {
+            "1 title": ii.get("title", ""),
+            "2 author": ii.get("author_name", ""),
+            "3 publisher": ii.get("publisher", ""),
+            "4 publish year": ii.get("publish_year", ""),
+            "5 ISBN": ii.get("isbn", ""),
+        }
+        result.append(info)
+
+    return result
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
